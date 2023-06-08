eb544bf07baebb135477f28b57ce63d124727b31
Minh Bình <tranminhbinhit@gmail.com>
2023-06-08 14:52:23 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ const ROUTES = [
         url: '/free',
         auth: false,
         creditCheck: false,
-        rateLimit: {
-            windowMs: 15 * 60 * 1000,
-            max: 5
-        },
+        // rateLimit: {
+        //     windowMs: 15 * 60 * 1000,
+        //     max: 5
+        // },
         proxy: {
             target: "https://api-main.byn.vn",
             changeOrigin: true,
